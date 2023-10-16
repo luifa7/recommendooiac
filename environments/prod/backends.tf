77d2996c1bdcf24c1022849059802ad5d6ef1e88
L Alvarez <luis.alvarez@digiventus.com>
2023-10-17 00:29:47 +0200

    Add rg to backends

--- environments/prod/backends.tf
+++ environments/prod/backends.tf
@@ -3,5 +3,6 @@ terraform {
     storage_account_name = "rcmdsharedsa"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
+    resource_group_name  = "rcmd-shared"  
   }
 }
