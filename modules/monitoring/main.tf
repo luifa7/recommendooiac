069ea9efe5c52d710ef3db9d4cbe7f3c7060949b
L Alvarez <luis.alvarez@digiventus.com>
2023-10-17 11:13:16 +0200

    Add workspace id to app insights

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -9,4 +9,5 @@ resource "azurerm_application_insights" "app_insights" {
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "web"
+  workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
 }
